5a0b783535b74e7a93a2aba783a7dbedaa1d3368
Chojan Shang <psiace@apache.org>
2024-03-04 01:08:30 +0800

    chore: make fmt happy
    Signed-off-by: Chojan Shang <psiace@apache.org>

--- examples/addition.rs
+++ examples/addition.rs
@@ -3,10 +3,7 @@ use tissue::{run, Input};
 fn main() {
     run(
         |x: Vec<f32>| x.iter().sum(),
-        &[
-            Input::Number(234.289),
-            Input::Number(235.6),
-        ],
+        &[Input::Number(234.289), Input::Number(235.6)],
     )
     .expect("Could not run");
 }
